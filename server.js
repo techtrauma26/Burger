3184eba87d777d95393c7c2326361df2f4adb524
Vyjoo Frank <vlakshm26@gmail.com>
2019-06-02 00:32:09 -0400

    directory created

---
+++ server.js
@@ -0,0 +1,28 @@
+ // ==============================================================================
+// DEPENDENCIES
+// ==============================================================================
+var express = require("express");
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// ==============================================================================
+// Tells node that we are creating an "express" server
+var app = express();
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+// Sets up the Express app to handle data parsing
+var public = require("path").join(__dirname,"app/public");
+app.use(express.static(public));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// ================================================================================
+// ROUTER
+// ======================================ds==========================================
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+// =============================================================================
+// LISTENER
+// =============================================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
