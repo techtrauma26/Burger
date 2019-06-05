81c29dbcce8f97a1295012f8de1c532344eb06c8
Vyjoo Frank <vlakshm26@gmail.com>
2019-06-05 04:06:05 -0400

    CREATED AND UPDATED

--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,26 @@
-// Import the ORM to create functions that will interact with the database.
+// *********************************************************************************
+// MODELS- BURGER.JS - THIS FILE INTERACTS WITH THE DATABASE 
+// *********************************************************************************
 var orm = require("../config/orm.js");
 
 var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
+  all: function (cb) {
+    orm.all("burgers", function (res) {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
-      cb(res);
-    });
+  create: function (name, cb) {
+    orm.create("burgers", [
+      "burger_name", "devoured"
+    ], [
+        name, false
+      ], cb);
   },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
+  update: function (id, cb) {
+    var condition = "id=" + id;
+    orm.update("burgers", {
+      devoured: true
+    }, condition, cb);
   }
 };
-
-// Export the database functions for the controller (catsController.js).
 module.exports = burger;
