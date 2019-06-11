f55a0ae1d02ebab4bbbb6d45235207c97ca6fe5a
Vyjoo Frank <vlakshm26@gmail.com>
2019-06-11 17:05:46 -0400

    updates

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
+var public = require("path").join(__dirname,"public");
+app.use(express.static(public));
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
